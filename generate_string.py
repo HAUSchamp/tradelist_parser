7650f91b6d0590963825ef2acd14f9decac6afca
HAUSchamp <108960827+HAUSchamp@users.noreply.github.com>
2024-10-25 12:02:28 -0400

    refactor search code, fix substring bugs
    Substring bug: modifiers that are substrings of mon (e.g. "ice" in "jellicent" and mon which are substrings of other mon (e.g. "mew" in "mewtwo" or "abra" in "kadabra" were incorrectly being double-counted

--- generate_string.py
+++ generate_string.py
@@ -1,59 +1,83 @@
-#######
+############################
 # User-modifiable paramaters
 
+# Constant to add to each string (e.g. exclude shinies or alr. traded mon)
 string_const = "&!traded&!shiny&!#&!favorite&!rocket&!4*"
 
+# Include a mon's evolutionary line by default?
 include_evo_line = True
 
+# Paste a plaintext list of mon here, can have other text too
 trade_list = """
-
 PASTE TRADE LIST HERE
-
 """.lower()
 
-#######
+############################
 
-evo = "+"*include_evo_line
-
-search_string = ""
-
-for mon in mon_list:
-	if mon in trade_list:
-		search_string += ","+evo+mon
-
+# In-game search modifiers
 modifiers = ["male", "female", 
 "normal", "fire", "water", "electric", "grass", "ice", "fighting", "poison", "ground", "flying", "psychic", "bug", "rock", "ghost", "dragon", "dark", "steel", "fairy", 
 "kanto", "johto", "hoenn", "sinnoh", "unova", "kalos", "alola", "galar", "hisui", "paldea", 
 "dynamax", "gigantamax", 
 "--", "+"]
 
-delimiters = [";", ":", "\n"] #comma implicit
+# Map alternate spellings for modifiers
+mod_maps = {"paldean":"paldea", "hisuian":"hisui", "unovan":"unova", "alolan":"alola"}
 
+# Delimiters for pairing modifiers with mon names
+delimiters = [";", ":", "\n"] # comma implicit
+
+# Make all possible delimiters a comma
 for d in delimiters:
 	trade_list = trade_list.replace(d,",")
 
+# Add "+" to each species by default?
+evo_line_default = "+"*include_evo_line
+
+# Build search string
+
+search_string = ""
 mod_string = ""
-no_line_mon = []
-yes_line_mon = []
 
-for chunk in trade_list.split(','):
-	for mod in modifiers:
-		if mod in chunk: # jellicent bug
-			for mon in mon_list: # abra/mew bug
-				if mon in chunk:
-					if mod == "--":
-						no_line_mon.append(mon)
-					elif mod == "+":
-						yes_line_mon.append(mon)
-					else:
-						mod_string += "&!" + evo + mon + "," + mod
+# Loop through comma-delimited chunks to identify modifier + mon combos
+for chunk in trade_list.split(","):
 
-for mon in no_line_mon: # only search for a specific mon in the evolutionary line
-	search_string = search_string.replace(",+" + mon + ",", "," + mon + ",")
-	search_string = search_string.replace(",+" + mon + "&", "," + mon + "&")
+	# Refresh the modifications and mon per chunk
+	chunk_mods = []
+	chunk_mon = ""
 
-for mon in yes_line_mon: # search for any mon in the evolutionary line
-	search_string = search_string.replace("," + mon + ",", ",+" + mon + ",")
-	search_string = search_string.replace("," + mon + "&", ",+" + mon + "&")
+	# Adding space lets users put "+" and "--" keywords next to the mon name
+	chunk = chunk.replace("--", "-- ").replace("+", "+ ")
 
-print(search_string[1:] + string_const + mod_string)
+	# Loop through all whitespace- or underscore-delimited bits
+	for subchunk in chunk.replace("_", " ").split():
+
+		if subchunk in modifiers: # found a modifier
+			chunk_mods.append(subchunk)
+		elif subchunk in mod_maps.keys(): # found alternate modifier spelling
+			chunk_mods.append(mod_maps[subchunk])
+		elif subchunk in mon_list: # found mon name
+			chunk_mon = subchunk
+
+	# Only do anything if chunk contains a mon
+	if len(chunk_mon) > 0: 
+		
+		# Start with default incl/exclusion of line
+		chunk_evo_line = evo_line_default 
+
+		# Apply modifiers to the final string
+		for mod in chunk_mods:
+			if mod == "--": # exclude evos
+				chunk_evo_line = ""
+			elif mod == "+": # include evos
+				chunk_evo_line = "+"
+			else: # all other modifiers
+				mod_string += "&!" + chunk_evo_line + chunk_mon + "," + mod
+		# Add new mon to string
+		search_string += chunk_evo_line + chunk_mon + ","
+
+		# Q: What happens if a user puts "+" and "--" keywords?
+		# A: Get better users
+
+# Build the final string
+print(search_string[:-1] + string_const + mod_string)
