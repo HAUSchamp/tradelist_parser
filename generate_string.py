45dea3db24650b85878b0aca653f7e8d20cc414e
HAUSchamp <108960827+HAUSchamp@users.noreply.github.com>
2025-08-15 14:16:11 -0400

    add "up to" search, close #2

--- generate_string.py
+++ generate_string.py
@@ -1,6 +1,30 @@
 from tradelist_parser.mon_family_map import mon_family_map
+def resolve_mon(mon, fam_mon_map):
+	"""
+	Resolves name name of family to base Pokemon name, if applicable.
+	Otherwise returns name given.
+	"""
+	if "_UPTO_" in mon:
+		added_mon = []
+		family, stop_at_mon = mon.split("_UPTO_")
+		for m in fam_mon_map[family]:
+			added_mon.append(m)
+			if stop_at_mon == m:
+				break
+		return added_mon
+	elif "FAMILY" in mon:
+		return ["+"+fam_mon_map[mon][0]]
+	else:
+		return [mon]
 
-def generate_trade_list(trade_list = "", string_const = "&!traded&!shiny&!favorite&!rocket&!4*&!#", include_evo_line = True):
+def generate_trade_list(trade_list = "", string_const = "&!traded&!shiny&!favorite&!rocket&!4*&!#", evo_line_default = "+"):
+	"""
+	Function to generate trade strings. `trade_list` is a string 
+	including mon names and modifiers, `string_const` is a constant
+	appended to the end of the string, and `evo_line_default` is
+	"+" (include all family members), "^" (include the line up to the
+	specified mon), and "--" (only include that species).
+	"""
 	trade_list = trade_list.lower()
 	
 	# Generate mon list from mon -> family map
@@ -21,7 +45,7 @@ def generate_trade_list(trade_list = "", string_const = "&!traded&!shiny&!favori
 	"dynamax", "gigantamax",
 	"costume",
 	"xxs", "xxl",
-	"--", "+"]
+	"--", "+", "^"]
 	
 	# Map alternate spellings for modifiers
 	mod_maps = {"paldean":"paldea", "hisuian":"hisui", "unovan":"unova", "alolan":"alola", "galarian":"galar"}
@@ -36,13 +60,16 @@ def generate_trade_list(trade_list = "", string_const = "&!traded&!shiny&!favori
 	for d in delimiters:
 		trade_list = trade_list.replace(d,",")
 	
-	# Add "+" to each species by default?
-	evo_line_default = "+"*include_evo_line
-	
 	# Build search string
-	
 	mon_to_search = []
 	search_modifiers = {}
+
+	# Check evo line default
+	if evo_line_default == "-":
+		evo_line_default = "--"
+		print("replacing '-' with '--' as the evolutionary line default")
+	elif not evo_line_default in ["--", "+", "^"]:
+		raise Exception("evo_line_default is "+str(evo_line_default)+" but must be in ['--', '+', '^']")
 	
 	# Loop through comma-delimited chunks to identify modifier + mon combos
 	for chunk in trade_list.split(","):
@@ -51,8 +78,8 @@ def generate_trade_list(trade_list = "", string_const = "&!traded&!shiny&!favori
 		chunk_mods = []
 		chunk_mon = ""
 	
-		# Adding space lets users put "+" and "--" keywords next to the mon name
-		chunk = chunk.replace("--", " -- ").replace("+", " + ")
+		# Adding space lets users put "+", "--", and "^" keywords next to the mon name
+		chunk = chunk.replace("--", " -- ").replace("+", " + ").replace("^", " ^ ")
 	
 		# Loop through all whitespace- or underscore-delimited bits
 		for subchunk in chunk.replace("_", " ").split():
@@ -80,18 +107,19 @@ def generate_trade_list(trade_list = "", string_const = "&!traded&!shiny&!favori
 	
 			# Apply modifiers to the final string
 			for mod in chunk_mods:
-				if mod == "--": # exclude evos
-					chunk_evo_line = ""
-				elif mod == "+": # include evos
-					chunk_evo_line = "+"
+				if mod in ['--', '+', '^']: # line modifier
+					chunk_evo_line = mod
 				else: # all other modifiers
 					non_evo_mods.append(mod)
 			if len(chunk_mods) == 0:
 				non_evo_mods.append("unmodified")
 	
-			# If searching for anything in the family, add to the family list
-			if chunk_evo_line: 
+			# If searching for anything in the family, add the family
+			if chunk_evo_line == "+": 
 				new_mon = mon_family_map[chunk_mon]
+			# If searching up to a particular evo, add the family and mon
+			elif chunk_evo_line == "^":
+				new_mon = mon_family_map[chunk_mon] + "_UPTO_" + chunk_mon
 			# Or if searching for only that mon stage, add it to the mon list
 			else: 
 				new_mon = chunk_mon
@@ -108,17 +136,18 @@ def generate_trade_list(trade_list = "", string_const = "&!traded&!shiny&!favori
 			# A: Get better users
 	
 	# Build the final string
-	def resolve_mon(mon, fam_mon_map):
-		"""
-		Resolves name name of family to base Pokemon name, if applicable.
-		Otherwise returns name given.
-		"""
-		if "FAMILY" in mon:
-			return("+"+fam_mon_map[mon][0])
-		else:
-			return(mon)
-	
-	search_string = ",".join(sorted(list(set([resolve_mon(mon, family_mon_map) for mon in mon_to_search]))))
+	print(mon_to_search)
+	all_mon_list = []
+	for mon in mon_to_search:
+		all_mon_list += resolve_mon(mon, family_mon_map)
+
+	print(all_mon_list)
+	all_mon_set = []
+	for mon in all_mon_list:
+		if mon not in all_mon_set and "+"+mon not in all_mon_set:
+			all_mon_set.append(mon)
+	print(all_mon_set)
+	search_string = ",".join(sorted(all_mon_set))
 	
 	# Build the string of search modifiers
 	mod_string = ""
@@ -130,7 +159,7 @@ def generate_trade_list(trade_list = "", string_const = "&!traded&!shiny&!favori
 			mod_errors.append("multiple modifiers found, " + str(mod_list) + ", ignoring modifiers for " +k)
 		else:
 			if mod_list[0] != "unmodified":
-				mod_string += "&!" + resolve_mon(k, family_mon_map) + "," + mod_list[0]
+				mod_string += "&!".join([""]+[m + "," + mod_list[0] for m in resolve_mon(k, family_mon_map)])
 	
 	if len(search_string) > 0: # only print if any mon found
 		print(search_string + mod_string + string_const)
@@ -142,4 +171,4 @@ if __name__ == "__main__":
 	my_trade_list = """
  	PASTE TRADE LIST HERE
  	"""
-	generate_trade_list(my_trade_list, string_const = "&!traded&!shiny&!favorite&!rocket&!4*&!#", include_evo_line = True)
+	generate_trade_list(my_trade_list, string_const = "&!traded&!shiny&!favorite&!rocket&!4*&!#", evo_line_default = "+")
